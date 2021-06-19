bb23094130fbb9f206e13ef16e8e60defa6637a1
sandy1066 <techbrain.sk.1996@gmail.com>
2021-06-19 22:44:50 +0530

    Add project files.

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=PC-SANDY\\SQLEXPRESS;Initial Catalog=Samurai-App-Db;Integrated Security=True;Pooling=False");
+        }
+    }
+}
---
+++ SamuraiApp.UI/Program.cs
@@ -0,0 +1,39 @@
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+
+        private static void Main(string[] args)
+        {
+            _context.Database.EnsureCreated();
+            GetSamurais("Before Adding: ");
+            AddSamurai();
+            GetSamurais("After Adding: ");
+            Console.Write("Press Any Key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sandeep"};
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai Count is: {samurais.Count}");
+            foreach(var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
+        }
+    }
+}
